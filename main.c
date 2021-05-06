6ec1328cc999fc3bc1c7787ab0af656d785b3569
David Wandratsch <david.wandratsch@htl-wels.at>
2021-05-06 10:50:23 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+//***********************************************************************
+//
+//                  H T L - W E L S   /   Klasse 3BHIT
+//
+//-----------------------------------------------------------------------
+// Programm         : Liste
+//
+// Ersteller        : David Wandratsch
+//
+// Erstelldatum     : 2021-05-06
+//
+// Abgabedatum      : 2021-05-18
+//
+// Kurzbeschreibung : Arbeiten mit einfach-verketteten Listen
+//
+// GitHub Repository: https://github.com/WandratschDavid/linkedLists
+//***********************************************************************
+
+// Including libraries
+#include <stdio.h>
+
+// main program
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
